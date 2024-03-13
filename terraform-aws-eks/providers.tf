51eee1ce149ebb8f33588f434ad3241fc95c42a8
naresh9000 <naresh.naresh.k7@gmail.com>
2024-03-13 14:11:23 +0530

    some changes

--- terraform-aws-eks/providers.tf
+++ terraform-aws-eks/providers.tf
@@ -1,7 +1,6 @@
 provider "aws" {
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
-  region     = var.region
+
+  region = "ap-south-2"
 }
 
 terraform {
--- terraform-aws-eks/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-aws_access_key_id     = "$TF_VAR_aws_access_key_id"
-aws_secret_access_key = "$TF_VAR_aws_secret_access_key"
-region                = "$TF_VAR_region"
